0ea200b5e6cef418eaae214528dcbf1cdd113d67
michaelbart <michaeljbart@me.com>
2017-09-04 10:01:23 -0400

    Initial Commit

---
+++ Telepic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Telepic
+//
+//  Created by Michael Bart on 8/24/17.
+//  Copyright © 2017 Telepic LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
